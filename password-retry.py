bf3c5d7931522bb77b37f0b67f567742c4478141
Claire Lin <claire.lin10@gmail.com>
2022-05-03 23:25:54 +0800

    revison- teacher's version

--- password-retry.py
+++ password-retry.py
@@ -1,14 +1,15 @@
 password = 'a123456'
-i = 0
-while i < 3:
-    pw = input('輸入密碼: ')
+i = 3                           #剩餘次數
+while i > 0:                    #沒有剩餘次數不進入迴圈
+    i = i - 1
+    pw = input('請輸入密碼: ')
     if pw == password:
-        print('登入成功')
+        print('登入成功！')      #逃出迴圈
         break
     else:
-        if i == 2:
-            break
+        print('密碼錯誤！')
+        if i > 0:
+            print('還有', i, '次機會')
         else:
-            print('密碼錯誤！還有', 2-i, '次機會')
-            i = i + 1
+            print('帳號暫時無法使用，請聯繫您的管理員')
         
